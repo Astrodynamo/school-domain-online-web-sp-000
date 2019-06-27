20c54873e64c4356d21053cae6936d22ea7ba2ac
Amanda King <amapickell@gmail.com>
2019-06-27 01:21:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,14 @@
 class School
+  attr_reader :name, :roster
   
   def initialize(name)
     @name = name
     @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if !@roster.include?(grade) @roster[grade] = []
+    @roster[grade] << name
   end
   
 end
